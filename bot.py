f9f551897c7091725897c1105acb40e78a8b4148
Piligreem <cry.warface@yandex.ru>
2022-08-22 14:25:28 +0300

    add logging

--- bot.py
+++ bot.py
@@ -2,25 +2,37 @@ import telebot
 from config import TOKEN
 
 from gtts import gTTS
+from loguru import logger
 
 
 bot = telebot.TeleBot(TOKEN)
 
-
-@bot.message_handler(commands=['start', 'help'])
-def handle_start_help(message):
-	bot.send_message(message.chat.id, text='Hello!')
+logger.add('./logs/applog.log', level='DEBUG')
+logger.info('Start bot')
 
 
-@bot.message_handler(content_types=['text'])
-def handle_text(message):
-    text = message.text
-    obj = gTTS(text, lang='ru')
-    obj.save('/tmp/voice.ogg')
-    voice = open('/tmp/voice.ogg', 'rb')
-
-    bot.send_voice(message.chat.id, voice)
+try:
+    @bot.message_handler(commands=['start', 'help'])
+    def handle_start_help(message):
+	    bot.send_message(message.chat.id, text='Hello!')
 
 
-bot.infinity_polling()
+    @bot.message_handler(content_types=['text'])
+    def handle_text(message):
+        text = message.text
+        obj = gTTS(text, lang='en')
+        obj.save('/tmp/voice.ogg')
+        voice = open('/tmp/voice.ogg', 'rb')
+
+        bot.send_voice(message.chat.id, voice)
+
+
+    bot.infinity_polling()
+
+except Exception as error:
+    logger.info(error)
+
+
+logger.info('Stop bot')
+
 
--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 from dotenv import load_dotenv
 
-
+# create dir for voice messages
 if not os.path.exists('./tmp'):
     os.mkdir('./tmp')
+# create dir for logs
+if not os.path.exists('./logs'):
+    os.mkdir('./logs')
 
 # load env varibale
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
-
-# absolute path to bot dir 
-THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
 # Bot token
 TOKEN = os.getenv('TOKEN')
